0bfacee8dd847bfdb23c5153a253ee2a2dd101a0
viktor.stanisik <viktorstanisic@yahoo.com>
2022-06-10 11:25:49 +0200

    addedTwilioConsoleApp

---
+++ SMSAppTwilio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+{
+
+    string accountSid = Environment.GetEnvironmentVariable("TwilioSID");
+    string authToken = Environment.GetEnvironmentVariable("TwilioAuth");
+    string text = "Aco Random Code Generator:";
+    TwilioClient.Init(accountSid, authToken);
+    Random randomNumber = new Random();
+    var message = MessageResource.Create(
+        body: text + " " + randomNumber.Next().ToString(),
+        from: new Twilio.Types.PhoneNumber("+16067157328"),
+        to: new Twilio.Types.PhoneNumber("+38970367241")
+
+    );
+
+    Console.WriteLine(message.Body);
+}
+
